ced8a162c9f46dc002aa4d95be592f843e783253
Karan-Kumar-Mishra <emiltokaranmishra@gmail.com>
2024-08-18 21:04:39 +0530

    Custom React

---
+++ build-basic-react/script.js
@@ -0,0 +1,8 @@
+let root= document.getElementById('root');
+
+reactElement=document.createElement('a');
+reactElement.href="https://react.dev/"
+reactElement.target="_blank"
+reactElement.innerHTML="Custom React"
+
+root.appendChild(reactElement)
